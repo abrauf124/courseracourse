52ae88191230cbaba419dd986930e84463e66b92
unknown <rifurauf124@gmail.com>
2020-09-21 13:07:14 +0530

    greetingenerator commitng

---
+++ mod4_solution/SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window){
+var helloSpeaker = {};
+helloSpeaker.speak=function speak(name) {
+  console.log(speakWord + " " + name);
+}
+var speakWord = "Hello";
+window.helloSpeaker=helloSpeaker;
+})(window);
